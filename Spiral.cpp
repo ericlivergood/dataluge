b4dd1f036acccb60d7edb73be5f5b0a9060f6c7d
ericlivergood <eric.livergood@blackstone.com>
2013-11-22 13:02:40 -0500

    transfering the dataz

--- Spiral.cpp
+++ Spiral.cpp
@@ -10,7 +10,7 @@
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
 
-
+#define DEFAULT_BUFLEN 512
 #include "Spiral.h"
 
 Spiral::Spiral(void)
@@ -24,14 +24,95 @@ Spiral::Spiral(void)
 
 Spiral::~Spiral(void)
 {
+	closesocket(Source);
+	closesocket(Dest);
+	WSACleanup();
 }
 
 HRESULT Spiral::Initialize()
 {
+	ConnectTo(&Source, SourceAddr, SourcePort);
+	ConnectTo(&Dest, DestAddr, DestPort);
+	return 0;
+}
+
+HRESULT Spiral::ConnectTo(SOCKET* Socket, LPCSTR Address, LPCSTR Port)
+{
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	WSADATA wsaData;
+    
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;
+
+	int r;
+	
+    // Initialize Winsock
+    r = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (r != 0) {
+        printf("WSAStartup failed with error: %d\n", r);
+        return 1;
+    }
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+	r = getaddrinfo(Address, Port, &hints, &result);
+    if ( r != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", r);
+        WSACleanup();
+        return 1;
+    }
+
+	Socket = &ConnectSocket;
 	return 0;
 }
 
 HRESULT Spiral::Transfer()
 {
+	HRESULT r, s;
+    char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	do{
+		r = recv(Source, recvbuf, recvbuflen, 0);
+
+		if(r > 0)
+		{
+			s = send(Dest, recvbuf, recvbuflen, 0);
+			if(s != r)
+			{
+				printf("send failed with error: %d\n", WSAGetLastError());
+				NetworkingCleanup();
+				return 1;
+			}
+			else
+			{
+				printf("Pushed %d bytes\n", s);
+			}
+		}
+		else if(r == 0)
+		{
+			printf("Connection Closed.\n", s);
+		}
+		else
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+		}
+
+	} while( r > 0 );
+
+	NetworkingCleanup();
+	return 0;
+}
+
+HRESULT Spiral::NetworkingCleanup(void)
+{
+	closesocket(Source);
+	closesocket(Dest);
+	WSACleanup();
 	return 0;
 }
--- Spiral.h
+++ Spiral.h
@@ -3,7 +3,8 @@
 #pragma once
 //The "spiral" is the track on which one luges.  Kinda looks like a pipe.
 //This class is intended to facilitate to transfer between two of our VDISockets, or a pipe.
-//Super simple implementation for now.
+//Super simple implementation for now:
+//	No buffering, no name resolution; just connect to both sides call recv then send
 class Spiral
 {
 public:
@@ -13,9 +14,15 @@ public:
 	HRESULT Transfer();
 
 private:
+	HRESULT NetworkingCleanup(void);
+	
+	static HRESULT ConnectTo(SOCKET* Socket, LPCSTR Address, LPCSTR Port);
+
 	LPCSTR SourcePort;
 	LPCSTR SourceAddr;
 	LPCSTR DestPort;
 	LPCSTR DestAddr;
+	SOCKET Source;
+	SOCKET Dest;
 };
 
